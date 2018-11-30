352958b8e44eb1c96d682ff03d491add0dcc5f88
Ali <alifelanv@gmail.com>
2018-11-29 20:29:12 -0600

    Created system

---
+++ sistema/sistema.py
@@ -0,0 +1,32 @@
+class System:
+
+    # Creates our system with the values sent from the client
+    def __init__(self, s, mm, q, rm, sm, p):
+        self.scheduling = s
+        self.memory = mm
+        self.quantum = q
+        self.realMemory = rm
+        self.swapMemory = sm
+        self.pageSize = p
+        self.pid = 1
+        self.process = {}
+
+    # Creates process, s is the process size in bytes
+    def createProcess(self, s):
+        pass
+
+    # Returns virtual address of process pid at v
+    def getAddress(self, pid, v):
+        pass
+
+    # Adds quantum to current process
+    def quantum(self):
+        pass
+
+    # Kills pid
+    def fin(self, pid):
+        pass
+
+    # Ends simulation
+    def end(self):
+        pass
